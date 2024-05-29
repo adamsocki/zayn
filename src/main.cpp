da13d5dd59562b7abca326493a0caaebc94f0237
adamsocki <socki.adam@gmail.com>
2024-05-28 19:44:22 -0500

    windows init

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,37 @@
-#define GL_SILENCE_DEPRECATION
 #include <GLFW/glfw3.h>
-#include <iostream>
 
-
-
-#include "zayn.cpp"
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
-
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
-int main()
+int main(void)
 {
-    // Initialize GLFW
-    if (!glfwInit())
-    {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
+    GLFWwindow* window;
 
-    // Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hello OpenGL", NULL, NULL);
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
-        std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    // Make the window's context current
+    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    // Set the viewport
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // Render loop
+    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        // Input
-        processInput(window);
-
-
-        ZaynUpdate();
-        // Render
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Swap buffers
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
+
+        /* Poll for and process events */
         glfwPollEvents();
     }
 
-    // Clean up and exit
-    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
-}
-
-void processInput(GLFWwindow *window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
 }
